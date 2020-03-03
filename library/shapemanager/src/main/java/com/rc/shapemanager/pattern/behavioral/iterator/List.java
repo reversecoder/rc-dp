b7eb34b8a28867af01c253d3762428668e21f73d
Md. Rashadul Alam <rashed.droid@gmail.com>
2020-03-03 20:43:44 +0600

    removed shapemanager library

--- library/shapemanager/src/main/java/com/rc/shapemanager/pattern/behavioral/iterator/List.java
+++
@@ -1,9 +0,0 @@
-package com.rc.shapemanager.pattern.behavioral.iterator;
-
-/**
- * @author Md. Rashadul Alam
- * Email: rashed.droid@gmail.com
- */
-public interface List<E> {
-    Iterator<E> iterator();
-}
--- library/shapemanager/src/main/java/com/rc/shapemanager/pattern/behavioral/state/State.java
+++
@@ -1,9 +0,0 @@
-package com.rc.shapemanager.pattern.behavioral.state;
-
-/**
- * @author Md. Rashadul Alam
- * Email: rashed.droid@gmail.com
- */
-public interface State {
-    public void performAction(StateManager stateManager);
-}
